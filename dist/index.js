86fce9feab7d5d62682393bf3e9ac0c9655bd97f
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-01-02 00:17:24 +0800

    basic

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//----------basic----------
+const str = 'Alan';
+let userName = 'Helen';
+userName = 'Henry';
+let age = 27;
+let boo = true;
+let boo = 290;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLDJCQUEyQjtBQUMzQixNQUFNLEdBQUcsR0FBVSxNQUFNLENBQUM7QUFDMUIsSUFBSSxRQUFRLEdBQVUsT0FBTyxDQUFDO0FBQzlCLFFBQVEsR0FBRyxPQUFPLENBQUM7QUFFbkIsSUFBSSxHQUFHLEdBQVUsRUFBRSxDQUFDO0FBQ3BCLElBQUksR0FBRyxHQUFXLElBQUksQ0FBQztBQUN2QixJQUFJLEdBQUcsR0FBRyxHQUFHLENBQUEifQ==
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+//----------basic----------
+const str:string = 'Alan';
+let userName:string = 'Helen';
+let age:number = 27;
