3ba3a627ca22f914d5d06c69d88f52a65eb5b0f1
AlanChou1995 <99056343+AlanChou1995@users.noreply.github.com>
2023-03-07 23:25:48 +0800

    typescript basic

--- src/index.ts
+++ src/index.ts
@@ -1,278 +1,44 @@
-// ----------basic----------
-const str = 'Alan';
-let str1: string;
-str1 = 'Helen';
+// -------- 基本類型 --------
 
-const num = 1000;
-const boo = true;
-const n = null;
-const un = undefined;
+// 型別
+const str: string = 'Alan'
+const num: number = 123
+const boo: boolean = true
+const n: null = null
+const un: undefined = undefined
 
-const test: any = true;
+// 陣列
+const ary: string[] = ['a', 'b']
+const ary2: string[][] = [['aa', 'bb']]
 
-
-
-// Array
-const arr: string[] = ['a', 'b'];
-const arr2: string[][] = [['aa', 'bb']];
-
-
-
-// Components
-const tuple: [string, number, boolean] = ['Alan', 27, true];
+// 元組
+const tuple: [number, string, boolean] = [1, 'a', true]
+const tuple2: [string, string][] = [['a', 'b']]
 
 
 
-// ----------Enum----------
-enum api {
-    SUCCESS = 0,
-    FAIL = -1,
-    STREAMING = 1,
+// -------- Enum 枚舉 --------
+
+enum LiveStatus {
+  SUCCESS = 0,
+  FAIL = -1,
+  STREAMING = 1
 }
 
-const apiStatus = api.STREAMING;
+let staus = LiveStatus.SUCCESS
 
 
 
-// ----------Union----------
-let aaa: number | string;
-aaa = 1000;
-aaa = 'str';
+// -------- Union --------
+
+let aaa: number | string
+aaa = 123
+aaa = 'Alan'
 
 
 
-// ----------type----------
-type A = number | string;
-type B = boolean | number;
-let a1: A;
-a1 = '23';
-a1 = 23;
-let b1: B;
-b1 = true;
-b1 = 22;
+// -------- type --------
 
-
-
-// ----------interface----------
-interface User {
-    name: string;
-    age: number;
-    email?: string;
-    status?: boolean;
-};
-interface User {
-    password: string;
-}
-
-const obj2: User = {
-    name: 'Helen',
-    age: 23,
-    password: '123456'
-}
-
-
-
-// ----------Object----------
-type Card = {
-    name: string;
-    desc: string;
-};
-const obj: Card = {
-    name: 'Alan',
-    desc: '....',
-};
-
-
-
-//----------function----------
-//parameter
-function hello(a: number, b: number): number {
-    return a + b;
-};
-function saySomeContent(name: string, content: string) {
-    return name + content;
-};
-
-
-
-//----------Arrow Function----------
-const saySomething = (name: string, str: string): string => {
-    return name + str;
-};
-
-
-
-//----------unknown----------
-type Data = {
-    "userId": number;
-    "id": number;
-    "title": string;
-    "completed": boolean;
-}
-
-async function getData() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const data = await res.json() as Data;
-    console.log(data);
-};
-
-
-const data1: Data = {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-};
-
-//if data1 is dynamic
-type Beta = {
-    name: string;
-}
-const Beta = data1 as unknown as Beta;
-
-
-
-//----------Class----------
-// private
-// public
-// protected
-
-class Live {
-    public roomName: string
-    private id: string
-    protected name: string
-
-    constructor(roomName: string, id: string, name: string) {
-        this.roomName = roomName;
-        this.id = id;
-        this.name = name;
-    }
-}
-const live1 = new Live('No.01', '0001', 'Alan');
-console.log(live1);
-
-class CarLive extends Live {
-    constructor(roomName: string, id: string, name: string) {
-        super(roomName, id, name)
-    }
-
-    start() {
-        super.roomName;
-    }
-}
-
-interface CarProps {
-    name: string;
-    age: number;
-    start: () => void;
-}
-
-class Car implements CarProps {
-    name: string;
-    age: number;
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-
-    start() { }
-}
-
-
-
-//----------Generic----------
-function print<T>(data: T) {
-    console.log('data', data);
-};
-
-print<number>(999);
-print<string>('Alan');
-print<boolean>(true);
-
-class Print<T> {
-    data: T;
-    constructor(d: T) {
-        this.data = d;
-    }
-};
-
-const p = new Print<string>('Alan');
-const p1 = new Print<number>(999);
-console.log('p', p);
-console.log('p1', p1);
-
-
-
-//----------Utility----------
-//Record
-interface CatInfo {
-    age: number;
-    breed: string;
-};
-type CatName = 'miffy' | 'boris' | 'mordred';
-//key value
-const cats: Record<CatName, CatInfo> = {
-    miffy: { age: 10, breed: 'Persian' },
-    boris: { age: 5, breed: 'Maine Coon' },
-    mordred: { age: 2, breed: 'British Shorthair' }
-};
-
-console.log(cats);
-
-
-interface UserInfo {
-    user_email?: string;
-    user_password: string;
-    user_name: string;
-    user_age: number;
-    user_status?: boolean;
-}
-
-type UserName = 'Alan' | 'Helen' | 'Kevin';
-
-const users: Record<UserName, UserInfo> = {
-    Alan: {
-        user_email: 'alan@gmail.com',
-        user_password: '123456',
-        user_name: 'Alan',
-        user_age: 27,
-        user_status: true
-    },
-    Helen: {
-        user_email: 'helen@gmail.com',
-        user_password: '123456',
-        user_name: 'Helen',
-        user_age: 23,
-        user_status: true
-    },
-    Kevin: {
-        user_email: 'kevin@gmail.com',
-        user_password: '123456',
-        user_name: 'Kevin',
-        user_age: 30,
-        user_status: true
-    },
-
-}
-console.log(users);
-
-
-//Pick
-type UserPick = Pick<UserInfo, 'user_name' | 'user_age'>;
-
-const obj5: UserPick = {
-    user_name: 'Alan',
-    user_age: 27,
-}
-console.log(obj5);
-
-
-//Omit
-type UserOmit = Omit<UserInfo, 'user_password'>;
-
-const obj6: UserOmit = {
-    user_name: 'Helen',
-    user_age: 23,
-}
-console.log(obj6);
+type A = number | string
+let a1:A = 'Alan'
+let a2:A = 123
